f34573f0d2513fde79e5fc4e290efc2c77b81e10
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2026-01-03 18:25:50 +0900

    feat: さわらび明朝の適用(#4)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,11 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
+import { Sawarabi_Mincho } from 'next/font/google';
 import './globals.css';
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
+const sawarabiMincho = Sawarabi_Mincho({
+  variable: '--font-sawarabi-mincho',
   subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
+  weight: ['400'],
 });
 
 export const metadata: Metadata = {
@@ -23,10 +19,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="ja">
+      <body className={`${sawarabiMincho.variable} antialiased`}>
         {children}
       </body>
     </html>
