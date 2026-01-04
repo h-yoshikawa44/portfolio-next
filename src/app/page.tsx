ab8857f995f0231d8e14314faa1de91c69076b80
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2026-01-04 16:40:02 +0900

    feat: トップページをざっと作った(#4)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,59 @@
 import Image from 'next/image';
+import myIcon from '@/assets/images/my-icon.png';
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{' '}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{' '}
-            or the{' '}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{' '}
-            center.
-          </p>
+    <main className="flex min-h-screen w-full flex-col items-center justify-between sm:items-start">
+      <header className="bg-brand-primary h-100 w-full px-14 py-22">
+        <h1 className="bg-white text-5xl inline-block p-1">
+          h-yoshikawa44's Portfolio
+        </h1>
+        <p className="text-3xl mt-4 bg-white inline-block">
+          よしのポートフォリオ
+        </p>
+      </header>
+      <section className="bg-brand-secondary w-full px-14 py-14">
+        <h2 className="text-white text-2xl header-double-line mb-2">
+          当サイトについて
+        </h2>
+        <p className="text-white">
+          Web Developer である h-yoshikawa44 のポートフォリオサイトです。
+          <br />
+          経験スキルや個人開発の履歴、各種技術活動に関するアカウントのリンクなどを集約しています。
+        </p>
+      </section>
+      <section className="bg-brand-secondary w-full px-14 py-14">
+        <h2 className="text-white text-2xl header-double-line mb-2">
+          管理人プロフィール
+        </h2>
+        <div className="p-4 bg-brand-primary rounded-xl text-white">
+          <div className="grid grid-cols-[1fr_4fr]">
+            <div className="p-1 rounded-[50%] bg-white w-26 h-26 flex justify-center items-center">
+              <Image
+                src={myIcon}
+                alt="myIcon"
+                width={88}
+                height={64}
+                priority
+              />
+            </div>
+            <div className="flex flex-col justify-center gap-y-2">
+              <p className="text-lg">Hitomi Yoshikawa</p>
+              <p className="text-sm">よし（h-yoshikawa44）</p>
+            </div>
+          </div>
+          <div className="mt-4">
+            <p>Web Developer。</p>
+            <br />
+            <p>
+              2017/10からエンジニアをやっています。
+              <br />
+              3年ほどサーバサイドメインでしたが、フロントエンドに転向して、現在はフロントエンドエンジニアとして仕事しています。
+            </p>
+            <p>比較的慣れているのは TypeScript × React、Next.js あたり。</p>
+          </div>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+      </section>
+    </main>
   );
 }
