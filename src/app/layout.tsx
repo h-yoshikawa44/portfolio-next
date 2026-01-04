ede67cb0cda9f2515246650ead957ff0139f6376
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2026-01-04 18:38:04 +0900

    feat: ヘッダーとフッターをコンポーネント化(#4)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from 'next';
 import { Sawarabi_Mincho } from 'next/font/google';
-import Image from 'next/image';
-import Link from 'next/link';
-import myIcon from '@/assets/images/my-icon.png';
+import { Footer } from '@/common/Footer';
+import { Header } from '@/common/Header';
 import './globals.css';
 
 const sawarabiMincho = Sawarabi_Mincho({
@@ -24,36 +23,9 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <body className={`${sawarabiMincho.variable} antialiased`}>
-        <header className="sticky top-0 z-10 md:px-14 py-3 px-4 from-brand-start to-brand-end bg-linear-to-b flex justify-between">
-          <Link href="/">
-            <div className="p-1 rounded-[50%] bg-white w-12 h-12 flex justify-center items-center">
-              <Image
-                src={myIcon}
-                alt="myIcon"
-                width={44}
-                height={32}
-                priority
-              />
-            </div>
-          </Link>
-          <nav>
-            <ul className="flex gap-x-4 text-white items-center h-full">
-              <li>
-                <Link href="/skill">Skill</Link>
-              </li>
-              <li>
-                <Link href="/my_products">MyProducts</Link>
-              </li>
-              <li>
-                <a href="https://changeofpace.site">Blog</a>
-              </li>
-            </ul>
-          </nav>
-        </header>
+        <Header />
         {children}
-        <footer className="from-brand-start to-brand-end bg-linear-to-b py-3 text-center text-white">
-          h-yoshikawa44 Built with Next.js.
-        </footer>
+        <Footer />
       </body>
     </html>
   );
---
+++ src/common/Footer.tsx
@@ -0,0 +1,7 @@
+export function Footer() {
+  return (
+    <footer className="from-brand-start to-brand-end bg-linear-to-b py-3 text-center text-white">
+      h-yoshikawa44 Built with Next.js.
+    </footer>
+  );
+}
