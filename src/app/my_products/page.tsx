ae4c11d92a5270e712a20b0147ca31fda3250ba6
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2026-01-12 18:42:58 +0900

    feat: 個人成果物ページをざっと作った(#6)

---
+++ src/app/my_products/page.tsx
@@ -0,0 +1,58 @@
+import { HeaderText } from '@/common/HeaderText';
+import { Hero } from '@/common/Hero';
+import { SectionBase } from '@/common/SectionBase';
+import { SubHeaderText } from '@/common/SubHeaderText';
+import { EVENT_PRODUCTS, MANAGEMENTS } from '@/data/myProducts';
+import { ProductItem } from '@/feature/product/ProductItem';
+
+export default function MyProducts() {
+  return (
+    <main className="flex min-h-screen w-full flex-col items-center justify-between">
+      <Hero mainTitle="My Products" subTitle="個人で作成した成果物など" />
+      <SectionBase>
+        <HeaderText>Web</HeaderText>
+        <div className="flex flex-col gap-y-8">
+          <section className="mt-2">
+            <SubHeaderText>運営</SubHeaderText>
+            <ul className="mt-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+              {MANAGEMENTS.map((management) => {
+                return (
+                  <li key={management.name}>
+                    <ProductItem
+                      name={management.name}
+                      description={management.description}
+                      ProductImg={management.ProductImg}
+                      productLink={management.productLink}
+                      repositoryUrl={management.repositoryUrl}
+                    />
+                  </li>
+                );
+              })}
+            </ul>
+          </section>
+          <section>
+            <SubHeaderText>イベント参加</SubHeaderText>
+            <small className="mt-2 block text-invert text-sm">
+              ※web1week...1週間で web サービスを作るハッカソンイベント。
+            </small>
+            <ul className="mt-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+              {EVENT_PRODUCTS.map((eventProduct) => {
+                return (
+                  <li key={eventProduct.name}>
+                    <ProductItem
+                      name={eventProduct.name}
+                      description={eventProduct.description}
+                      ProductImg={eventProduct.ProductImg}
+                      productLink={eventProduct.productLink}
+                      repositoryUrl={eventProduct.repositoryUrl}
+                    />
+                  </li>
+                );
+              })}
+            </ul>
+          </section>
+        </div>
+      </SectionBase>
+    </main>
+  );
+}
